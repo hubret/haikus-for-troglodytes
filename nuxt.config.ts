c6bb918d51b82a255a58a71b410154aec0a4e20f
Hubert <hubhcsi@gmail.com>
2022-11-07 23:34:46 -0500

    pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ['~/assets/style/reset.css']
+  css: ['~/assets/style/reset.css'],
+  modules: [
+    '@kevinmarrec/nuxt-pwa'
+  ],
+  pwa: {
+    workbox: {
+      enabled: true
+    }
+  }
 })
