db340cb098582f0b49ba5d7738195b19bfc5df74
Hubert <hubhcsi@gmail.com>
2024-10-14 20:09:58 -0400

    prevent pinch 2 zoom

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,12 @@ export default defineNuxtConfig({
       title: 'Haikus for Troglodytes',
       link: [
         { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ],
+      meta: [
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        }
       ]
     }
   },
